8ce50617543663e200ae87309db2dea58ad8d4d8
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-06-10 13:55:31 +0000

    Update Player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -15,4 +15,33 @@ public class Player{
             this.hand = new Card[0];
         }
     }
-}
+
+    public Player(String name){
+        this.name = name;
+        this.hand = new Card[0];
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Card[] getHand(){
+        return this.hand;
+    }
+
+    public int size(){
+        return this.hand.length;
+    }
+
+    public void draw(Deck deck){
+        Card card = deck.draw();
+        if (card == null){
+            return;
+        }
+        Card[] newHand = new Card[this.hand.length + 1];
+        for (int i = 0; i < this.hand.length; i++){
+            newHand[i] = this.hand[i];
+        }
+        newHand[this.hand.length] = card;
+        this.hand = newHand;
+    }
