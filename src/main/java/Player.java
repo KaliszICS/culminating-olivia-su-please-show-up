b6c9b9e6a1d7eafec4639c1eb124db2bfa2eb31e
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-06-11 02:28:50 +0000

    Update

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * 
@@ -9,7 +10,8 @@
 
 public class Player{
     private String name;
-    private Card[] hand;
+    private ArrayList<Card> frontHand;
+    private ArrayList<Card> backHand;
 
 
 /**
@@ -17,26 +19,12 @@ public class Player{
 * @param name is the name of player
 * @param hand the cards in the player's hand
 */
-   public Player(String name, Card[] hand){
-        this.name = name;
-        if (hand != null){
-            this.hand = new Card[hand.length];
-            for (int i = 0; i < hand.length; i++){
-                this.hand[i] = hand[i];
-            }
-        }
-        else{
-            this.hand = new Card[0];
-        }
-    }
+   public Player(String name, ArrayList<Card> hand, ArrayList<Card> backHand){
 
-/**
-* Takes a name while creating a default empty hand of cards
-* @param name is the name of the player
-*/
-    public Player(String name){
         this.name = name;
-        this.hand = new Card[0];
+        this.frontHand = hand;
+        this.backHand = backHand;
+
     }
 
 /**
@@ -49,10 +37,19 @@ public class Player{
 
 /**
 * returns the cards in the player's hand
-* @return returns the player's hand
+* @return returns the player's hand at the front
 */
-    public Card[] getHand(){
-        return this.hand;
+    public ArrayList<Card> getFrontHand(){
+        return this.frontHand;
+    }
+
+/**
+ * returns the backside of the cards in the player's hands
+ * @return returns the player's hand at the back
+ */
+
+    public ArrayList<Card> getBackHand(){
+        return this.backHand;
     }
 
 /**
@@ -60,7 +57,7 @@ public class Player{
 *@return returns the size of the player's hand
 */
     public int size(){
-        return this.hand.length;
+        return this.frontHand.size();
     }
 
 /**
@@ -68,16 +65,11 @@ public class Player{
 * @param deck the deck that the card is taken from
 */
     public void draw(Deck deck){
-        Card card = deck.draw();
-        if (card == null){
+        Card drawnCard = deck.draw();
+        if (drawnCard == null){
             return;
         }
-        Card[] newHand = new Card[this.hand.length + 1];
-        for (int i = 0; i < this.hand.length; i++){
-            newHand[i] = this.hand[i];
-        }
-        newHand[this.hand.length] = card;
-        this.hand = newHand;
+        this.hand.add(drawnCard);
     }
 
 /**
@@ -85,26 +77,14 @@ public class Player{
 * Returns true if the card exists in hand, false otherwise
 * @param card is the card that will be discarded
 * @param discardPile is the pile the card will be discarded into
-* @return returns true if the card exists and was discarded, false otherwise
+* @return returns the card taken out of the hand
 */
-    public boolean RemoveCard(Card card, DiscardPile discardPile){
+    public Card RemoveCard(Card card, Deck discardPile){
         if (card == null || this.hand.length == 0){
-            return false;
+            return null;
         }
-        for (int i = 0; i < this.hand.length; i++){
-            if (this.hand[i].equals(card)){
-                discardPile.addCard(card);
-                Card[] newHand = new Card[this.hand.length - 1];
-                for (int a = 0, b = 0; a < this.hand.length; a++){
-                    if (a != i){
-                        newHand[b++] = this.hand[a];
-                    }
-                }
-                this.hand = newHand;
-                return true;
-            }
-        }
-        return false;
+            this.hand.remove(card);
+            return card;
     }
 
 
@@ -120,11 +100,11 @@ public class Player{
 */
     public String toString(){
         String person = this.name;
-        if (this.hand.length == 0){
+        if (this.hand.size == 0){
             return person;
         }
-        for (int i = 0; i < this.hand.length; i++){
-            person += ", " + this.hand[i].toString();
+        for (int i = 0; i < this.hand.size; i++){
+            person += ", " + this.hand.get(i).toString();
         }
         return person;
     }
