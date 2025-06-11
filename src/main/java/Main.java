90522ca08730c308f3114e805cf44ac3b5a6dbf1
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-06-11 13:53:54 +0000

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+/**
+ * The Main class is the UNO game
+ * It makes a deck, lets the players organize their cards, and play the game with UNO flip rules 
+ * @author Kelly Fu, Olivia Su, Katherine Zhou, Cindy Pan
+ * @version 17.0.1
+ */
+
 public class Main {
 
 	public static void main(String args[]) {
@@ -5,7 +12,8 @@ public class Main {
 		
 		//System.out.println(d.toString());
 		d.shuffle();
-	
+	//prints the size of the deck
+	//prints the back cards of your hand
 	Player pl = new Player("help", d);
 	System.out.println(pl.size());
 	System.out.println(pl.getBackHand().toString());
