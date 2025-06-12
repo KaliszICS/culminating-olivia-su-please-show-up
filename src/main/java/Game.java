ade88dadc9bba2e6065f1308bc1716141a4a6013
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-06-12 14:15:32 +0000

    Update message

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,10 @@
+/**
+ * this class represents a game being started and should be able to execute different player's actions 
+ * and account for different turns, card effects, and more
+ * @author Kelly Fu, Olivia Su, Katherine Zhou, Cindy Pan
+ * @version 17.0.1
+ */
+
 class Game{
     private int currentTurn;
     private String currentColour;
@@ -10,6 +17,13 @@ class Game{
     private boolean isPlayerTurn;
     private Card topCard;
 
+    /**
+     * this consctrutor takes in players, opponents, the deck being used, and the pile of cards we're playing in
+     * @param player the player
+     * @param opponent the computer we're playing against
+     * @param deck the deck of cards we're drawing from
+     * @param pile the pile of cards we are playing our cards to
+     */
      public Game(Player player, Opponent opponent, Deck deck, Deck pile) {
         this.currentTurn = 0;
         this.player = player;
@@ -24,24 +38,50 @@ class Game{
         System.out.println("Game started! First card: " + topCard);
     }
 
+    /**
+     * this method gets the current colour of the top card
+     * @return the current colour
+     */
     public String getCurrentColour(){
         return this.currentColour;
     }
 
+    /**
+     * this method gets the current type of the top card
+     * @return the current type
+     */
     public String getCurrentType(){
         return this.currentType;
     }
 
+    /**
+     * this method gets what turn counter its on
+     * @return the current turn
+     */
     public int getCurrentTurn(){
         return this.currentTurn;
     }
+
+    /**
+     * this method gets the boolean of the players turn
+     * @return is it the players turn (true if yes false if no)
+     */
     public boolean getIsPlayerTurn(){
         return this.isPlayerTurn;
     }
+    /**
+     * this method sets/alters the players turn
+     * @param set the boolean value being used to alter the players turn
+     */
     public void setIsPlayerTurn(boolean set){
         this.isPlayerTurn = set;
     }
 
+    /**
+     * this method should check if the player have cards to play 
+     * if not, it will draw a card
+     * @return a string that tells the player whats happening
+     */
     public String playable(){
         if(this.isPlayerTurn){
             this.isPlayerTurn = !isPlayerTurn;
@@ -65,6 +105,13 @@ class Game{
         
 
     }
+
+    /**
+     * this method reads what the computer or the player is trying to play and add it to the play pile
+     * 
+     * @param play the card being played
+     * @return a message to tell you what card is played
+     */
     public String playCard(Card play){
         if(this.isPlayerTurn){
             this.isPlayerTurn = !this.isPlayerTurn;
@@ -81,6 +128,10 @@ class Game{
         
     }
 
+    /**
+     * this method checks if you or the computer have run out of cards, therefore winning
+     * @return a message telling you if you have won, the computer has won, or no one has.
+     */
     public String gameWon(){
         if(player.size()==0){
             return "You won!";
